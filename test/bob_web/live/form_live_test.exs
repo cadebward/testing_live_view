712e1f786b134639b7cab2f4b0175029ec2c07a5
Cade Ward <cadebward@gmail.com>
2020-02-18 07:05:08 -0700

    add test

---
+++ test/bob_web/live/form_live_test.exs
@@ -0,0 +1,31 @@
+defmodule BobWeb.FormLiveTest do
+  @moduledoc """
+  `BobWeb.ConnCase` sets the following variables:
+
+    * @endpoint
+    * Routes
+
+  Hints:
+
+    * path to the LiveView: `Routes.live_path(@endpoint, FormLive)`
+    * static test: `html = conn |> get(path) |> html_reponse(200)`
+    * live test: `{:ok, view, _html} = live(conn, path)`
+    * `render_*` sends events
+    * `Floki.parse_document!(html) |> Floki.find(selector)`
+  """
+
+  use BobWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  alias BobWeb.FormLive
+
+  describe "FormLive" do
+    test "renders a form"
+
+    test "renders an error when email is invalid"
+
+    test "renders an error when email is not present"
+
+    test "creates a user when form is valid"
+  end
+end
